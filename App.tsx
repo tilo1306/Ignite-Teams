b60f139d2abbb725cf5fab4749a0dcb12ad9ee81
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-15 09:38:02 -0300

    feat: Criando Componente Header

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from "@components/Loading";
@@ -9,13 +10,15 @@ export default function App() {
     Roboto_400Regular, Roboto_700Bold
   })
 
-
-
-
-
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Groups /> : <Loading />}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
