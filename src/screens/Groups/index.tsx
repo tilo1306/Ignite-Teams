0bb8e48d95ce73296569633ad8ba9a4a9d1cff83
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-16 20:49:10 -0300

    feat: Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import { Header } from '@components/Header'
-import { Container, } from './styles'
 import { HighLight } from '@components/HighLight'
 import { GroupCard } from '@components/GroupCard'
+import { Container, } from './styles'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
     return (
         <Container>
             <Header />
@@ -11,7 +14,17 @@ export function Groups() {
                 title='Tumars'
                 subTitle='Jogue com a sua turma'
             />
-            <GroupCard title='Galera do Ignite' />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) =>
+                (
+                    <GroupCard title={item} />
+                )
+                }
+
+            />
 
         </Container>
     )
