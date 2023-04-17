624b19393e933027e16fba8084704d0c14687178
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-16 21:49:23 -0300

    feat: Criando Componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard'
 import { Container, } from './styles'
 import { FlatList } from 'react-native'
 import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
@@ -32,7 +33,7 @@ export function Groups() {
                 )
                 }
             />
-
+            <Button title='Criar nova turma' />
         </Container>
     )
 }
