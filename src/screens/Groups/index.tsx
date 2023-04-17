b7e2f0d1c73b08ad2b7a269d7ce2a911ebd79b17
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-16 21:18:11 -0300

    feat: Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { HighLight } from '@components/HighLight'
 import { GroupCard } from '@components/GroupCard'
 import { Container, } from './styles'
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
@@ -23,7 +24,13 @@ export function Groups() {
                     <GroupCard title={item} />
                 )
                 }
-
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message=" Qua tal cadastrar a primeira turma?"
+                    />
+                )
+                }
             />
 
         </Container>
