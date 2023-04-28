6e3c2a1678eb38f358cf84cb19bd8db2ce1c6544
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-28 15:06:41 -0300

    style:  Using the CSS Helper

--- src/components/HighLight/styles.ts
+++ src/components/HighLight/styles.ts
@@ -1,21 +1,24 @@
-import styled from "styled-components/native";
-
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
-    width: 100%;
-    margin: 32px 0;
-`
+  width: 100%;
+  margin: 32px 0;
+`;
 
 export const Title = styled.Text`
+  ${({ theme }) => css`
     text-align: center;
-    font-size: ${({theme})=> theme.FONT_SIZE.XL}px;
-    font-family: ${({theme})=> theme.FONT_FAMILY.BOLD};
-    color: ${({theme})=> theme.COLORS.WHITE};
-`
+    font-size:  theme.FONT_SIZE.XL}px;
+    font-family:  theme.FONT_FAMILY.BOLD};
+    color:  theme.COLORS.WHITE};
+  `}
+`;
 
 export const SubTitle = styled.Text`
+  ${({ theme }) => css`
     text-align: center;
-    font-size: ${({theme})=> theme.FONT_SIZE.MD}px;
-    font-family: ${({theme})=> theme.FONT_FAMILY.REGULAR};
-    color: ${({theme})=> theme.COLORS.GRAY_300};
-`
+    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+  `}
+`;
