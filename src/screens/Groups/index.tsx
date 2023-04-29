841e67f77f3e082556f5e1b1a7b9dfe672a43965
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-28 22:57:53 -0300

    chore: Installing Stack Navigator

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,39 +1,33 @@
-import { useState } from 'react'
-import { Header } from '@components/Header'
-import { HighLight } from '@components/HighLight'
-import { GroupCard } from '@components/GroupCard'
-import { Container, } from './styles'
-import { FlatList } from 'react-native'
-import { ListEmpty } from '@components/ListEmpty'
-import { Button } from '@components/Button'
+import { useState } from "react";
+import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
+import { Container } from "./styles";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
-    const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
-    return (
-        <Container>
-            <Header />
-            <HighLight
-                title='Tumars'
-                subTitle='Jogue com a sua turma'
-            />
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da Rocket",
+    "Amigos",
+  ]);
+  return (
+    <Container>
+      <Header />
+      <HighLight title="Tumars" subTitle="Jogue com a sua turma" />
 
-            <FlatList
-                data={groups}
-                keyExtractor={item => item}
-                renderItem={({ item }) =>
-                (
-                    <GroupCard title={item} />
-                )
-                }
-                contentContainerStyle={groups.length === 0 && { flex: 1 }}
-                ListEmptyComponent={() => (
-                    <ListEmpty
-                        message=" Qua tal cadastrar a primeira turma?"
-                    />
-                )
-                }
-            />
-            <Button title='Criar nova turma' />
-        </Container>
-    )
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message=" Que tal cadastrar a primeira turma?" />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+      <Button title="Criar nova turma" />
+    </Container>
+  );
 }
