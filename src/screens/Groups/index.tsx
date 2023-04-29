37c268c1b3e0632815bc74393d26179fc81507fb
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-28 23:27:30 -0300

    feat: Typing the routes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { GroupCard } from "@components/GroupCard";
-import { Container } from "./styles";
-import { FlatList } from "react-native";
-import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { ListEmpty } from "@components/ListEmpty";
+
+import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([
-    "Galera da Rocket",
-    "Amigos",
-  ]);
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
